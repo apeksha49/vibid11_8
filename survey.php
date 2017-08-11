ffb919cba0ea6de7e3d14b1a9ad66f9982f357b5
apeksha <apekshashah2727@gmail.com>
2017-08-11 15:29:17 +0530

    padding

---
+++ survey.php
@@ -0,0 +1,277 @@
+<!-- Add text here !-->
+<style id="media-query">
+    /* Client-specific Styles & Reset */
+ 
+	 #outlook p {
+        padding: 10px;
+    }
+    /* .ExternalClass applies to Outlook.com (the artist formerly known as Hotmail) */
+    .ExternalClass {
+        width: 100%;
+    }
+    .ExternalClass,
+    .ExternalClass p,
+    .ExternalClass span,
+    .ExternalClass font,
+    .ExternalClass td,
+    .ExternalClass div {
+        line-height: 100%;
+    }
+    #backgroundTable {
+        margin: 0;
+        padding: 0;
+        width: 100% !important;
+        line-height: 100% !important;
+    }
+    /* Buttons */
+    .button a {
+        display: inline-block;
+        text-decoration: none;
+        -webkit-text-size-adjust: none;
+        text-align: center;
+    }
+    .button a div {
+        text-align: center !important;
+    }
+    /* Outlook First */
+    body.outlook p {
+        display: inline !important;
+    }
+    /*  Media Queries */
+    @media only screen and (max-width: 500px) {
+        table[class="body"] img {
+            height: auto !important;
+            width: 100% !important;
+        }
+        table[class="body"] img.fullwidth {
+            max-width: 100% !important;
+        }
+        table[class="body"] center {
+            min-width: 0 !important;
+        }
+        table[class="body"] .container {
+            width: 95% !important;
+        }
+        table[class="body"] .row {
+            width: 100% !important;
+            display: block !important;
+        }
+        table[class="body"] .wrapper {
+            display: block !important;
+            padding-right: 0 !important;
+        }
+        table[class="body"] .columns, table[class="body"] .column {
+            table-layout: fixed !important;
+            float: none !important;
+            width: 100% !important;
+            padding-right: 0px !important;
+            padding-left: 0px !important;
+            display: block !important;
+        }
+        table[class="body"] .wrapper.first .columns, table[class="body"] .wrapper.first .column {
+            display: table !important;
+        }
+        table[class="body"] table.columns td, table[class="body"] table.column td, .col {
+            width: 100% !important;
+        }
+        table[class="body"] table.columns td.expander {
+            width: 1px !important;
+        }
+        table[class="body"] .right-text-pad, table[class="body"] .text-pad-right {
+            padding-left: 10px !important;
+        }
+        table[class="body"] .left-text-pad, table[class="body"] .text-pad-left {
+            padding-right: 10px !important;
+        }
+        table[class="body"] .hide-for-small, table[class="body"] .show-for-desktop {
+            display: none !important;
+        }
+        table[class="body"] .show-for-small, table[class="body"] .hide-for-desktop {
+            display: inherit !important;
+        }
+        .mixed-two-up .col {
+            width: 100% !important;
+        }
+    }
+    @media screen and (max-width: 500px) {
+        div[class="col"] {
+            width: 100% !important;
+        }
+    }
+    @media screen and (min-width: 501px) {
+        table[class="container"] {
+            width: 500px !important;
+        }
+    }
+</style>
+<table  style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tr style="width: 100%;">
+        <td style="text-align: center; padding: 10px;" width="100%">
+            <img src="http://tryvitalsleep.com/img/logo.gif" class="elIMG ximg">
+        </td>
+    </tr>
+</table>
+<table>
+    <tr>
+        <td>
+            <div style="text-align:center;">
+               <p style="font-size:20px;color:#BFD428"> Thank you! Your order has been placed. </p>
+               <p> Your order will be quickly shipped to you by USPS Mail. </p>
+               <p> Do not overheat or bite forcefully into the device. </p>
+               <p> We recommend carefully heating it in hot water for only 5-10 seconds.</p> 
+               <p> Please contact us at anytime at support@vitalsleep.com if you have any questions.</p>
+               <p> Follow all instructions that are included with your order. </p>
+               <p> *Returns require a Return Merchandise Authorization (RMA) number. </p>
+               <p> Discontinue use if you experience discomfort or pain. Improper use of device may void product warranty.</p>
+
+
+
+            </div>
+        </td>
+    </tr>
+</table>
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 15px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#aaaaaa;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#aaaaaa;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        <strong>Order Summary</strong>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top; max-width: 480px;"
+        align="center" width="100%" border="0"
+        cellpadding="0" cellspacing="0">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 5px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px"
+            align="center">
+            <div style="height: 1px;">
+                <table
+                        style="border-spacing: 0;border-collapse: collapse;vertical-align: top;border-top: 1px solid #BBBBBB;width: 100%"
+                        align="center" border="0"
+                        cellspacing="0">
+                    <tbody>
+                    <tr style="vertical-align: top">
+                        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top"
+                            align="center"></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+{php} if (isset($_SESSION['post_data']['not_set1'])): {/php}
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#555555;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#555555;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        1 - {php} echo $_SESSION['post_data']['not_set1']; {/php}  VitalSleep Mouthpiece $0.00 ($59.95 after 30 day trial) <br>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+{php}endif;{/php}
+{php} if (isset($_SESSION['post_data']['new_post_data']['not_set2']) && $_SESSION['post_data']['new_post_data']['extra'] == 'yes'): {/php}
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#555555;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#555555;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        1 - Bonus {php} echo $_SESSION['post_data']['new_post_data']['not_set2']; {/php} VitalSleep Mouthpiece $0.00 ($59.95 after 30 day trial) <br>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+{php}endif;{/php}
+{php}  if (isset($_SESSION['post_data']['extra-prod'])): {/php}
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#555555;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#555555;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        1 - Additional 1 Year Warranty $0.00 ($14.95 after 30 day trial) <br>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+{php} endif;{/php}
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#555555;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#555555;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        {php} if ($_SESSION['post_data']['shipping'] && $_SESSION['post_data']['shipping'] == 9.95): {/php}
+                        International Airmail Shipping
+                        {php} endif;{/php}
+                        {php} if ($_SESSION['post_data']['shipping'] && $_SESSION['post_data']['shipping'] == 7.95):{/php}
+                        USA First Class Shipping
+                        {php} endif;{/php} ${php} echo $_SESSION['post_data']['shipping']; {/php} <br>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<table
+        style="border-spacing: 0;border-collapse: collapse;vertical-align: top"
+        cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+    <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;padding-top: 10px;padding-right: 10px;padding-bottom: 10px;padding-left: 10px">
+            <div
+                    style="color:#555555;line-height:120%;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;">
+                <div
+                        style="font-size:12px;line-height:14px;color:#555555;font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;text-align:left;">
+                    <p style="margin: 0;font-size: 14px;line-height: 17px;text-align: center">
+                        Total ${php} echo $_SESSION['post_data']['amount']; {/php} <br>
+                    </p></div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
